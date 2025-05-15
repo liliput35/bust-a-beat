e5d086f0fd6303bcdf4b7c6541dd3416bbaa9f5d
Lorenz Ciocon <lorenzciocon@gmail.com>
2025-05-15 21:39:19 +0800

    Hearts UI Improved

--- Bust-A-Beat/Assets/Scripts/HeartUIController.cs
+++ Bust-A-Beat/Assets/Scripts/HeartUIController.cs
@@ -1,21 +1,51 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HeartUIController : MonoBehaviour
 {
-    public Animator heartAnimator;   // Animator controlling all heart visuals
-    public PlayerScript player;      // Reference to PlayerScript
-    public EnemyScript enemy;
+    public PlayerScript player;      // Reference to player
+    public EnemyScript enemy;        // Reference to enemy
+
+    public Image heartsUI;           // UI Image to display heart sprite
+
+    public Sprite fiveHealth;
+    public Sprite fourHealth;
+    public Sprite threeHealth;
+    public Sprite twoHealth;
+    public Sprite oneHealth;
+    public Sprite zeroHealth;
+
+    public bool showPlayerHealth = true; // Toggle this to switch between player/enemy health UI
 
     void Update()
     {
-        if (player != null && heartAnimator != null)
-        {
-            heartAnimator.SetFloat("Health", player.currentHealth);
-        }
+        int currentHealth = showPlayerHealth ? player.currentHealth : enemy.currentHealth;
+        UpdateHeartSprite(currentHealth);
+    }
 
-        if (enemy != null && heartAnimator != null)
+    void UpdateHeartSprite(int health)
+    {
+        switch (health)
         {
-            heartAnimator.SetFloat("Health", enemy.currentHealth);
+            case 5:
+                heartsUI.sprite = fiveHealth;
+                break;
+            case 4:
+                heartsUI.sprite = fourHealth;
+                break;
+            case 3:
+                heartsUI.sprite = threeHealth;
+                break;
+            case 2:
+                heartsUI.sprite = twoHealth;
+                break;
+            case 1:
+                heartsUI.sprite = oneHealth;
+                break;
+            case 0:
+            default:
+                heartsUI.sprite = zeroHealth;
+                break;
         }
     }
 }
