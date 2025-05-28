e0ab04623e283589e5bf8cd402633393aa71313f
Lorenz Ciocon <lorenzciocon@gmail.com>
2025-05-28 22:43:24 +0800

    cutscene audio

---
+++ Bust-A-Beat/Assets/SceneLoaderCut.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderCut : MonoBehaviour
+{
+    public float changeTime;
+    public string sceneName;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        changeTime -= Time.deltaTime;
+
+        if(changeTime <= 0)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
